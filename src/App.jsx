e3d0dedbc65366fa549845b9fec7d887b24be10d
ksai7991 <76088923+ksai7991@users.noreply.github.com>
2025-10-04 04:02:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -192,7 +192,7 @@ function App() {
             left: 0,
             width: "100%",
             height: "100%",
-            objectFit: "contain",
+            objectFit: "cover",
             zIndex: -1,
           }}
         >
